5ca748cfee538be5d89faf8cff0c1a754c8b8218
Dan Farrow <dan@zellement.com>
2025-01-30 11:19:20 +0000

    remove autoblocker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -174,18 +174,15 @@ export default defineNuxtConfig({
                 // }
             ],
             script: [
-                // ...(process.env.NODE_ENV === 'production'
-                //     ? [
-                //           {
-                //               src: 'https://web.cmp.usercentrics.eu/modules/autoblocker.js'
-                //           },
-                //           {
-                //               id: 'usercentrics-cmp',
-                //               src: 'https://web.cmp.usercentrics.eu/ui/loader.js',
-                //               'data-settings-id': 'MWxdC_lTYphdZO'
-                //           }
-                //       ]
-                //     : [])
+                ...(process.env.NODE_ENV === 'production'
+                    ? [
+                          {
+                              id: 'usercentrics-cmp',
+                              src: 'https://web.cmp.usercentrics.eu/ui/loader.js',
+                              'data-settings-id': 'MWxdC_lTYphdZO'
+                          }
+                      ]
+                    : [])
                 // {
                 //     hid: 'gtm-script',
                 //     innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${import.meta.env.GTM_ID}');`,
